f9ec45289cfcc98415138f30a2a09c806cb1d0a3
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-04 20:53:42 +0200

    Create unknown/unknown.js

---
+++ commands/unknown/unknown.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const config = require('../../config');
+
+module.exports = async (sock, msg, command, prefix) => {
+    const from = msg.key.remoteJid;
+
+    return await sock.sendMessage(from, {
+        text:
+`❌ *Unknown Command*
+
+The command *${prefix}${command}* does not exist.
+
+💡 Type *${prefix}menu* to view all available commands.
+
+> 🤖 Powered By *${config.botName}*`
+    }, {
+        quoted: msg
+    });
+};
