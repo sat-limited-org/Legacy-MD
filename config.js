5345c2576e2b79ea04096122cab0178412f14449
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-04 15:51:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ module.exports = {
     sessionName: 'session',
     sessionID: process.env.SESSION_ID || '',
     newsletterJid:
- '120363161513685998@newsletter', // Newsletter JID for menu forwarding
+ '120363408718616120@newsletter', // Newsletter JID for menu forwarding
     updateZipUrl: 'https://github.com/sat-limited-org/Legacy-MD/archive/refs/heads/main.zip', // URL to latest code zip for .update command
     
     // Sticker Configuration
