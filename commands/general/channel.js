7fa2296365d2d1fb6eef7ba29dd23cefe1274b67
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-04 17:00:01 +0200

    Update channel.js

--- commands/general/channel.js
+++ commands/general/channel.js
@@ -12,25 +12,33 @@ module.exports = {
     usage: ".channel",
 
     async execute(sock, msg, args, extra) {
-        await sock.sendMessage(
-            extra.from,
-            {
-                text: `📢 *OFFICIAL CHANNEL*
+        try {
+            await sock.sendMessage(
+                extra.from,
+                {
+                    text: `📢 *OFFICIAL CHANNEL*
 
 Follow our official WhatsApp Channel to stay updated!
 
 https://whatsapp.com/channel/0029Vb8A6Tz8qIzs2X2aFX3n`,
-                contextInfo: {
-                    forwardingScore: 999,
-                    isForwarded: true,
-                    forwardedNewsletterMessageInfo: {
-                        newsletterJid: config.newsletterJid || "120363161513685998@newsletter",
-                        newsletterName: config.ownerName || config.botName,
-                        serverMessageId: -1
+                    contextInfo: {
+                        forwardingScore: 999,
+                        isForwarded: true,
+                        forwardedNewsletterMessageInfo: {
+                            newsletterJid: config.newsletterJid,
+                            newsletterName: config.botName,
+                            serverMessageId: 1
+                        }
                     }
-                }
-            },
-            { quoted: msg }
-        );
+                },
+                { quoted: msg }
+            );
+        } catch (err) {
+            console.error("[CHANNEL]", err);
+
+            await sock.sendMessage(extra.from, {
+                text: "❌ Failed to send channel link."
+            }, { quoted: msg });
+        }
     }
 };
