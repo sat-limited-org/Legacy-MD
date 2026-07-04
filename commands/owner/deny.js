903b6a99ca7b928ca26e2f83b9d574ac0c62a0ab
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-04 22:31:17 +0200

    Create deny.js

---
+++ commands/owner/deny.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const dbPath = path.join(__dirname, '../../database/allowedGroups.json');
+
+module.exports = {
+    commands: ['deny', 'disallow'],
+    description: 'Remove this group from the allowed list.',
+    usage: '.deny',
+    permission: 'owner',
+    group: true,
+    private: false,
+
+    run: async (sock, message) => {
+        const from = message.key.remoteJid;
+
+        if (!from.endsWith('@g.us')) {
+            return sock.sendMessage(from, {
+                text: '❌ This command can only be used in groups.'
+            });
+        }
+
+        let groups = [];
+
+        if (fs.existsSync(dbPath)) {
+            groups = JSON.parse(fs.readFileSync(dbPath));
+        }
+
+        groups = groups.filter(id => id !== from);
+
+        fs.writeFileSync(dbPath, JSON.stringify(groups, null, 2));
+
+        await sock.sendMessage(from, {
+            text: '🚫 This group is no longer allowed to use the bot.'
+        });
+    }
+};
