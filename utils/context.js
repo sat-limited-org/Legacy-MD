304c5cb0d14fec63f2d6922751b7ea2e93461876
Simbarashe Augustus Tembo <tembosimbarasheaugustus@gmail.com>
2026-07-09 22:51:22 +0200

    Update context.js

--- utils/context.js
+++ utils/context.js
@@ -3,7 +3,7 @@ function newsletterCtx() {
         forwardingScore: 999,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363420252027000@newsletter', // replace with your channel ID
+            newsletterJid: '120363408718616120@newsletter', // replace with your channel ID
             newsletterName: 'SAT Limited',
             serverMessageId: 1
         }
